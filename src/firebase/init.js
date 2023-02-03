65a2304a910ffb8ccb79f2f62bc167864b50c5e6
Laurion <klauriondareich@gmail.com>
2023-02-03 14:59:33 +0100

    firebase config

---
+++ src/firebase/init.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import * as firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp
